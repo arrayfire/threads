093f91dcda921c60b982edefaed492d4592f60ca
Umar Arshad <umar@arrayfire.com>
2015-07-15 02:11:49 -0400

    STYLE: Formatting and docs for async_queue

--- async_queue.hpp
+++ async_queue.hpp
@@ -19,59 +19,62 @@ using std::function;
 using std::bind;
 
 class async_queue {
-  thread                  queue_thread;
-  queue<function<void()>> work_queue;
-  atomic<bool>            done;
-  mutex                   queue_mutex;
-  condition_variable      cond;
+    thread queue_thread;
+    queue<function<void()>> work_queue;
+    atomic<bool> done;
+    mutex queue_mutex;
+    condition_variable cond;
 
-  void
-  queue_runner() {
-    std::cout << "Starting: " << std::this_thread::get_id() << std::endl;
-    while(!done) {
-      unique_lock<mutex> lock(queue_mutex);
-      cond.wait(lock, [this] () {
-          return work_queue.empty() == false || done.load();
-        });
-      function<void()> func = work_queue.front();
-      func();
-      work_queue.pop();
+    void queue_runner() {
+        std::cout << "Starting: " << std::this_thread::get_id() << std::endl;
+        while (!done) {
+            unique_lock<mutex> lock(queue_mutex);
+            cond.wait(lock, [this]() {
+                return work_queue.empty() == false || done.load();
+            });
+            if(done) break;
+            function<void()> func = work_queue.front();
+            func();
+            work_queue.pop();
+        }
     }
-  }
+
 public:
-
-  template<typename F, typename... Args>
-  void
-  enqueue(F func, Args... args)
-  {
-    auto no_arg_func = std::bind(func, args...);
-    {
-      lock_guard<mutex> lock(queue_mutex);
-      work_queue.push(no_arg_func);
+    /// \brief Enqueues a new function onto the work queue
+    ///
+    /// \param func A function which will be enqueued on the work queue
+    /// \param args The argument of the funciton \p func
+    template <typename F, typename... Args>
+    void enqueue(F func, Args... args) {
+        auto no_arg_func = std::bind(func, args...);
+        {
+            lock_guard<mutex> lock(queue_mutex);
+            work_queue.push(no_arg_func);
+        }
+        cond.notify_one();
+        return;
     }
-    cond.notify_one();
-    return;
-  }
 
-  void
-  sync() {
-    std::cout << "Syncing" << std::endl;
-    while(work_queue.empty() == false);
-    std::cout << "Done Syncing" << std::endl;
-  }
+    /// \brief Blocks until all work has completed
+    ///
+    /// This function will block the calling thread until all of the queued
+    /// functions have completed
+    void sync() {
+        std::cout << "Syncing" << std::endl;
+        while (work_queue.empty() == false)
+            std::this_thread::yield();
+        std::cout << "Done Syncing" << std::endl;
+    }
 
-  async_queue()
-    : done(false)
-  {
-    thread tmp(&async_queue::queue_runner, this);
-    swap(queue_thread, tmp);
-  }
+    /// \brief Creates a new work queue
+    async_queue() : done(false) {
+        thread tmp(&async_queue::queue_runner, this);
+        swap(queue_thread, tmp);
+    }
 
-  ~async_queue()
-  {
-    done.store(true, std::memory_order_relaxed);
-    cond.notify_one();
-    queue_thread.join();
-  }
-
+    ~async_queue() {
+        done.store(true);
+        cond.notify_one();
+        queue_thread.join();
+    }
 };
